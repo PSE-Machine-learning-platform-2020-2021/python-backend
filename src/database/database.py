efc5df48a3f88b969a70c838432ff6609061aa92
Morridin <paulza@gmx.de>
2021-03-02 16:37:06 +0100

    Introduced some instance variables to Database class
    This leads to a more object orientated and less error prone behaviour from my point of view - you pass the data set ids from now on only once at object creation and after that all the values requested are stored so we do not have to recalculate them every time we request values again.

--- src/database/database.py
+++ src/database/database.py
@@ -19,23 +19,34 @@ class Database:
     This class bundles together all needed database accessing needed for current plan of python part.
     """
 
-    def __init__(self):
+    def __init__(self, data_set_ids: list[int]):
         """
         Creates an object of Database class based on configuration file.
+        :param data_set_ids: A list containing the database indices of all of the desired data sets for further
+                             processing.
         """
+        self._labels: dict[int, str] = {}
+        self._labels_reversed: dict[str, int] = {}
+        self.data_set_ids = data_set_ids
         config = ConfigReader()
         db_data = config.get_values("DB")
         self.data_base = mysql.connector.connect(*db_data)
+        self.data_sets: list[DataFrame] = []
+        self.sensor_type_ids: list[int] = []
 
-    def get_data_sets(self, data_sets: list[int]) -> list[pd.DataFrame]:
+    def get_data_sets(self) -> list[DataFrame]:
         """
         This method retrieves all datasets specified by parameter indices from the database specified in config file.
-        ATTENTION: This method does atm not load and append any labels to the data! But a label column is appended.
-        TODO: Change that.
-        :param data_sets: A list containing the database indices of all of the desired data sets for further processing.
+
+        It loads those data sets only once from the data base to save time, so you have to empty the data_sets field if
+        you wish to refresh the result.
+
         :return: A tuple containing all data rows found and matching one of the passed data set ids, grouped together
                  by their data set id into pandas Dataframe objects.
         """
+        if len(self.data_sets) > 0:
+            return self.data_sets
+
         cursor = self.data_base.cursor(dictionary=True)
         # With this query we select all data rows belonging to the given data sets together with their name and
         # the name of the sensor that was used for them.
@@ -44,8 +55,7 @@ class Database:
                           (SELECT sensorName FROM Sensor WHERE Sensor.sensorID = Datarow.sensorID) AS sensorName
                    FROM Datarow
                    WHERE datasetID = %s"""
-        result: list[DataFrame] = []
-        for i in data_sets:
+        for i in self.data_set_ids:
             # Execute query for every single dataset
             cursor.execute(query, i)
             data_set: dict[str, dict] = {}
@@ -61,32 +71,43 @@ class Database:
             # that all values are in correct ascending order by timestamp
             for key in data_set.keys():
                 data_set[key] = {x: np.NaN for x in sorted(times)} | data_set[key]
-            result.append(pd.DataFrame(data_set))
 
-        # Ensure that there is always a label column ...
-        for x in result:
-            if "label" not in x:
-                x["label"] = [np.NaN for x in x.index]
-        return result
+            if len(data_set) == 0:
+                self.data_set_ids.remove(i)
+            else:
+                ds = pd.DataFrame(data_set)
+                ds.id = i
+                self.data_sets.append(ds)
 
-    def get_sensors(self, data_sets: list[int]) -> list[int]:
+        for x in self.data_sets:
+            x["label"] = -1
+            self._get_labels()
+        return self.data_sets
+
+    def get_sensor_type_ids(self) -> list[int]:
         """
         This method retrieves information from the Sensor database table.
         This information contains the types of the sensors used in the
         data sets whose ids are passed as this function's parameter.
-        :param data_sets: A list containing the numeric ids of one or more data sets.
+
+        Please note that you have to reset the field sensor_type_ids in order to
+        refresh the result if you call that method twice.
+
         :return: A list of numbers representing sensor types used in the data sets passed by id
         """
+        if len(self.sensor_type_ids) > 0:
+            return self.sensor_type_ids
         cursor = self.data_base.cursor(dictionary=True)
         query = """SELECT sensorTypeID AS type
                    FROM Sensor 
                    WHERE sensorID 
                    IN (SELECT sensorID FROM Datarow WHERE datasetID in %s)"""
-        cursor.execute(query, tuple(data_sets))
+        cursor.execute(query, tuple(self.data_set_ids))
         result: set[int] = set()
         for row in cursor.fetchall():
             result |= row["type"]
-        return sorted(result)
+        self.sensor_type_ids = sorted(result)
+        return self.sensor_type_ids
 
     def get_stuff(self, classifier_id: int) -> tuple[Any, Any, list[int], dict[int, str]]:
         """
@@ -94,6 +115,7 @@ class Database:
         If the underlying database connector mechanics raise Errors or Exceptions while doing this, they are not
         handled here. If there is more than one result with the specified ID, a ValueError is raised, as the database
         is corrupted in this case.
+
         :param classifier_id: The id of the classifier in its database table.
         :return: A classifier object and a scaler object bundled together in a tuple.
         """
@@ -106,28 +128,66 @@ class Database:
             raise ValueError
         classifier = pickle.loads(result[0]["Classifier"])
         scaler = pickle.loads(result[0]["Scaler"])
-        sensors, labels_table = json.loads(result[0]["Sensors"]), json.loads(result[0]["LabelsTable"])
-        return classifier, scaler, sensors, labels_table
+        self.sensor_type_ids, self._labels = json.loads(result[0]["Sensors"]), json.loads(result[0]["LabelsTable"])
+        self._labels_reversed = {v: k for k, v in self._labels}
+        return classifier, scaler, self.sensor_type_ids, self._labels
 
-    def put_stuff(self, classifier, scaler, sensors, labels_table: dict[int, str] = None) -> int:
+    def put_stuff(self, classifier, scaler, sensors: list[int] = None) -> int:
         """
         This method puts a classifier, a scaler, the label-to-number association table and the list of sensor types
         that were used when collecting the data for the data sets used in the training of the ai model into the
         corresponding data base table.
+
         :param classifier: A classifier from sklearn. It is then pickled and stored.
         :param scaler: The scaler that was used to transform the training data for the classifier passed right before.
-        :param sensors: The sensors used for collecting the data of the data sets used as training data for the
-                        classifier passed as first parameter.
-        :param labels_table: A dict containing numbers as keys associating with label names. Those numbers which must be
-                             non-negative are used to classify input - or training - data by the ai model.
+        :param sensors: The ids of the types of the sensors used for collecting the data that was used to train the
+                        classifier passed as first parameter and the scaler passed as second one.
+                        If not passed, the list stored in this object is used, as long as it is not empty. In the latter
+                        case, the list is internally collected.
         :return: The Id of the classifier ("AI model ID").
         """
         query = """INSERT INTO Classifiers (Classifier, Scaler, Sensors, LabelsTable) VALUES (%s, %s, %s, %s)"""
         cursor = self.data_base.cursor()
+        self._get_labels()
+        if sensors is None:
+            sensors = self.get_sensor_type_ids()
         cursor.execute(query,
                        (pickle.dumps(classifier),
                         pickle.dumps(scaler),
                         json.dumps(sensors),
-                        json.dumps(labels_table)))
+                        json.dumps(self._labels)))
         self.data_base.commit()
         return cursor.lastrowid
+
+    def _get_labels(self) -> None:
+        """
+        This PRIVATE method is not meant to be called from outside the class.
+        It gathers information about labels on the data requested via this specific instance of this class.
+        It also applies them onto the data sets.
+
+        If there are no labels provided for the data in question, a value error is raised.
+        """
+        if len(self._labels) > 0:
+            return
+        query = """SELECT datasetID, name, start, end FROM Label WHERE datasetID IN %s"""
+        cursor = self.data_base.cursor(dictionary=True)
+        cursor.execute(query, tuple(self.data_set_ids))
+        label_names: set[str] = set()
+        result = cursor.fetchall()
+        if len(result) == 0:
+            raise ValueError
+        for row in result:
+            row["name"] = row["name"].strip().casefold().upper()  # This ensures unified all-uppercase format
+            label_names |= row["name"]
+        self._labels = {k: v for k, v in enumerate(sorted(label_names))}
+        self._labels_reversed = {v: k for k, v in enumerate(sorted(label_names))}
+        for ds in self.data_sets:
+            timestamps = list(ds.index)
+            for row in result:
+                if row["datasetID"] == ds.id:
+                    new_label = []
+                    for k, v in zip(timestamps, ds["label"]):
+                        new_label.append(self._labels_reversed[row["name"]] if row["start"] <= k <= row["end"] else v)
+                    ds["label"] = new_label
+                else:
+                    continue
