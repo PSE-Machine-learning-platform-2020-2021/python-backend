711621abe411055babbc6a93dfbfc829ebc54a78
Morridin <paulza@gmx.de>
2021-03-10 18:31:50 +0100

    Finished Tests - all are green.

--- test/test_buildModel.py
+++ test/test_buildModel.py
@@ -6,11 +6,13 @@ import io
 import random
 import unittest
 from contextlib import redirect_stdout
+from pickle import load
 from unittest import TestCase
 
 import pandas as pd
 
 from sklearn.dummy import DummyClassifier
+from sklearn.impute import SimpleImputer
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler, Normalizer
 
@@ -20,6 +22,7 @@ import sys
 
 
 class BuildModelTest(TestCase):
+
     def test_fetch_parameters(self):
         sys.argv.append("testFile.json")
         with open("testFile.json", "w") as f:
@@ -56,7 +59,7 @@ class BuildModelTest(TestCase):
 
     def test_choose_features(self):
         result = buildModel.choose_features(['MIN', 'ENERGY', 'FOURIER_TRANSFORM', 'Bullshit'])
-        self.assertCountEqual(result, ["minimum", "abs_energy", "???"])
+        self.assertCountEqual(result, ["minimum", "abs_energy"])
 
     def test_choose_scaler(self):
         result = buildModel.choose_scaler('MIN')
@@ -75,46 +78,49 @@ class BuildModelTest(TestCase):
         self.assertIsInstance(result, DummyClassifier().__class__)
 
     def test_create_time_slices_random_legal_both(self):
-        result = buildModel.create_time_slices([self.data], self.chunk, self.step)
+        result = buildModel.create_time_slices([self.data], self.imputator, self.chunk, self.step)
         self.assertEqual(len(result), 2)
         self.assertEqual(len(result[0]), self.length)
         self.assertEqual(len(result[0]), len(result[1]))
         self.assertLessEqual(len(result[0][0]), self.chunk)
 
-    def test_create_time_slices_empty_data_set_list(self):
-        self.assertEqual(buildModel.create_time_slices([]), ([], []))
-
     def test_create_time_slices_illegal_values(self):
-        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], 0)
-        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], self.chunk, self.chunk + 1)
-        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], step=0)
+        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], self.imputator, 0)
+        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], None, self.chunk, self.chunk + 1)
+        self.assertRaises(ValueError, buildModel.create_time_slices, [self.data], self.imputator, step=0)
 
-    @unittest.skip("TSFresh is not working as expected. Awaiting reparation.")
     def test_extract_features(self):
-        self.fail()
+        buildModel.extract_features(["minimum", "maximum", "variance", "mean"], [self.data.iloc[:1024, :-1]],
+                                    [1], self.imputator)
+        self.assertTrue(True)
 
-    def test_partition_data_legal(self):
-        x_data = [0 for _ in range(self.length)]
-        y_data = [random.randint(0, 42) for _ in range(self.length)]
-        result = buildModel.partition_data(x_data, y_data)
-        self.assertEqual(len(result), 4)
-        self.assertEqual(len(result[0]), len(result[1]))
-        self.assertEqual(len(result[2]), len(result[3]))
-        self.assertAlmostEqual(len(result[0]) / len(x_data), 0.8, 1)
-        self.assertEqual(len(x_data), len(result[0]) + len(result[2]))
+    def test_partition_data(self):
+        result = buildModel.partition_data(self.data)
+        self.assertEqual(result[0].shape[1] + 1, self.columns)
+        self.assertEqual(result[0].shape[0] + result[2].shape[0], self.data.shape[0])
+        self.assertAlmostEqual(result[0].shape[0] / self.data.shape[0], 0.8, 1)
 
-    def test_partition_data_illegal(self):
-        x_data = [0 for _ in range(self.length)]
-        y_data = [random.randint(0, 42) for _ in range(self.length // 2)]
-        self.assertRaises(ValueError, buildModel.partition_data, x_data, y_data)
+        percentage = random.random()
+        result = buildModel.partition_data(self.data, percentage)
+        self.assertEqual(result[0].shape[1] + 1, self.columns)
+        self.assertEqual(result[0].shape[0] + result[2].shape[0], self.data.shape[0])
+        self.assertAlmostEqual(result[0].shape[0] / self.data.shape[0], percentage, 1)
 
-    @unittest.expectedFailure
+        self.assertRaises(ValueError, buildModel.partition_data, None)
+        self.assertRaises(ValueError, buildModel.partition_data, self.data, 0)
+        self.assertRaises(ValueError, buildModel.partition_data, self.data, 1)
+        self.assertRaises(ValueError, buildModel.partition_data, self.data, -1)
+        self.assertRaises(ValueError, buildModel.partition_data, self.data, 42)
+
     def test_preprocess_data(self):
-        self.fail()
+        result = buildModel.preprocess_data(self.data.iloc[:, :-1], self.scaler)
+        self.assertIsNotNone(result)
+        self.assertEqual(type(result), pd.DataFrame)
 
-    @unittest.expectedFailure
     def test_train_classifier(self):
-        self.fail()
+        self.assertIsNone(buildModel.train_classifier(self.data.iloc[:, :-1],
+                                                      self.data["label"],
+                                                      self.classifier))
 
     def test_notify_server(self):
         f = io.StringIO()
@@ -127,22 +133,27 @@ class BuildModelTest(TestCase):
         """
         Prepares all the tests that need big amounts of data although.
         """
-        data = pd.read_csv("all_feature.csv")
-        self.data = pd.DataFrame(data)
+        data = pd.read_csv("exp12_user06.csv")
+        self.data = pd.DataFrame(data).fillna(-1)
         self.chunk = random.randint(1, 2 ** 8)
         self.step = random.randint(1, self.chunk)
         self.length = 1
+        self.imputator = SimpleImputer()
         while self.length * self.step < self.data.shape[0] - self.chunk:
             self.length += 1
         self.scaler = Normalizer()
         self.classifier = MLPClassifier()
         self.columns = self.data.shape[1]
+        self._file = open("temp", "rb")
+        self.extracted_data = load(self._file)
 
     def tearDown(self) -> None:
         """
         Clears up all resources.
         """
+        self._file.close()
         del self.data, self.chunk, self.step, self.length, self.scaler, self.classifier, self.columns
+        del self.extracted_data, self.imputator, self._file
 
 
 if __name__ == "__main__":
